737796e346f042dc5ce71d7d04b9e86e011fa6f7
lucasphill <lucaspscp@outlook.com>
2025-04-10 20:13:55 -0300

    Primeiro CRUD em Node.JS com express sem persistencia dos dados

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-import http from "http";
+//import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-  "/": "Curso de Node.js",
-  "/livros": "Entrei na rota livros",
-  "/autores": "Entrei na rota autores",
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" }); // cria o header da requisição http (NADA A VER com html)
-  res.end(rotas[req.url]); // retorna minhas rotas
-});
-
 // servidor ouvindo porta 3000
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Server listening");
 });
